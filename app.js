e46dabe9dc1f9e91265cca148ea62596a18c221c
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-16 17:49:32 +0300

    fix reponse message

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const app = express()
 app.use(express.json())
 
 app.post("/auth/register", async (req, res) => {
-    const {username, email, password} = req.body
+    const { username, email, password } = req.body
     if (!username || !email || !password) {
-        res.status(400).json({message: "body is missing fields"})
+        res.status(400).json({ message: "body is missing fields" })
     }
-    const newId = await register(username, email, password)
+    await register(username, email, password)
+    res.status(201).json({ message: "User registered successfully" }
+    )
 })
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({message: err.message})
-}) 
+    res.status(err.status || 500).json({ message: err.message })
+})
 
 
 app.listen(process.env.PORT, () => console.log("server is up and listening"))
